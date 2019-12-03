9a936ce939d72eae87a8a6f6444c22b4b36f2965
jcla490 <josh.clark@dnr.wa.gov>
2019-12-02 21:53:43 -0800

    minor updates

--- whiskey_list/static/whiskey_list/js/site.js
+++ whiskey_list/static/whiskey_list/js/site.js
@@ -1,15 +1,29 @@
 $(document).ready( function () {
-    $('#spirit_table').DataTable({
+
+    // Sums a column
+    jQuery.fn.dataTable.Api.register( 'sum()', function ( ) {
+        return this.flatten().reduce( function ( a, b ) {
+            if ( typeof a === 'string' ) {
+                a = a.replace(/[^\d.-]/g, '') * 1;
+            }
+            if ( typeof b === 'string' ) {
+                b = b.replace(/[^\d.-]/g, '') * 1;
+            }
+            return a + b;
+        }, 0 );
+    } );
+    
+    var table = $('#spirit_table').DataTable({
         paging:   false,
         order: [[ 0, "asc" ]],
         scrollX: true, 
-        dom: '<lfi<t>>',
+        dom: '<lf<i<"#total_whiskies">><t>>',
         language: {
             searchPlaceholder: "Search whiskies (and rums!)",
             search: "",
             zeroRecords: "I don't own this...yet!",
             infoEmpty: "Showing 0 whiskies?!? How can this be??",
-            info: "Showing _TOTAL_ of _MAX_ whiskies",
+            info: "Showing _TOTAL_ of _MAX_ unique whiskies",
             infoFiltered:   "",
         },
         columnDefs: [ {
@@ -21,9 +35,7 @@ $(document).ready( function () {
         },{
             targets: [ 2 ],
             orderData: [ 2, 1, 0]
-        },{
-            targets: [ 3 ],
-            orderData: [ 3, 0, 1]
-        } ]
-    })
+        }]
+    });
+
 });
